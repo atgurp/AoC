872d5ad133b944d7657e9410076e816061e525fa
gurpreetgit <gusingh0192@gmail.com>
2021-12-09 18:01:34 -0800

    Edited to clear out

--- day3.py
+++ day3.py
@@ -4,16 +4,5 @@ with open('day3text.txt') as file:
     for line in file:
         desc = line.rstrip()
         lst.append(desc)
-        
-
-print(lst)
-zeronum1 = 0
-onenum1 = 0 
-gamma_rate_lst = []
-for i in range(len(lst)):
-    num = (lst[i])
-
-    p = sum(5 for l in lst[i])
     
-    print(p)
 
